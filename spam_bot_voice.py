d62d424f0e8deb43e86ead3844d477f77268dfff
Brady Redding <44198185+BradyCodes@users.noreply.github.com>
2020-12-06 15:12:04 -0500

    Add files via upload

--- spam_bot_voice.py
+++ spam_bot_voice.py
@@ -3,6 +3,9 @@ import webbrowser as wb
 import time
 import pyttsx3
 import speech_recognition as sr
+import os
+
+os.system('clear')
 
 def speak(x):
     engine = pyttsx3.init()
@@ -26,7 +29,7 @@ def rec():
             elif y == "no" or y == "No" or y =="new" or y == "now":         # now,new --> no
                 speak("Say it again")
             else:
-                speak("Oh O you just said something out of the window.Answer to what is asked naah!")
+                speak("Please Answer with yes or no")
         return x
 def run():               # to cut the error that comes when we are very quick
     try:
@@ -36,30 +39,57 @@ def run():               # to cut the error that comes when we are very quick
         speak("Hold down! Please be a bit slow")
         rec()
 
+questions = [
+    "What Do You Want me to Spam?",
+    "What is the number of whom you'd like to spam?",
+    "How many times would you like to spam this message?"
+]
+spam_ans = 0
+num_ans = 0
+rep_ans = 0
+
+
+def askRecieve(q, type):
+    print(q)
+    speak(q)
+    if type == "spam_ans":
+        spam_ans= rec().capitalize().split(" ")
+    elif type == "num_ans":
+        num_ans = rec()
+        num_ans = int(num_ans.replace(" ",""))
+        num_ans = int(num_ans.replace("-",""))
+    elif type == "rep_ans":
+        rep_ans = int(rec())
+
 # welcome speech
 
 print(" Abrar's Whatsapp Spam Bot \n\n".center(75))
 speak("Welcome to Abrar's Whatsapp Spam Bot")
 # getting data from user
 
+askRecieve(questions[0], "spam_ans")
+askRecieve(questions[1], "num_ans")
+askRecieve(questions[2], "rep_ans")
+
+
 #QUESTION - I
-spam_ques ="--> What do you want me to spam?"
-print(spam_ques)
-speak(spam_ques)
-spam_ans= rec().capitalize().split(" ")       #  "how are you" = ["How","are","you"]
+# spam_ques ="--> What do you want me to spam?"
+# print(spam_ques)
+# speak(spam_ques)
+# spam_ans= rec().capitalize().split(" ")       #  "how are you" = ["How","are","you"]
 
-#QUESDTION -II
-num_ques = "--> What is the number to whom you want to text?"
-print(num_ques)
-speak(num_ques)
-num_ans = rec()
-num_ans = int(num_ans.replace(" ",""))     #   '123 4567 890' = 1234567890
+# #QUESDTION -II
+# num_ques = "--> What is the number to whom you want to text?"
+# print(num_ques)
+# speak(num_ques)
+# num_ans = rec()
+# num_ans = int(num_ans.replace(" ",""))     #   '123 4567 890' = 1234567890
 
-#QUESTION-III
-rep_ques ="--> How many times do you want to send this text?"
-print(rep_ques)
-speak(rep_ques)
-rep_ans = int(rec())                  #    "202" = 202
+# #QUESTION-III 
+# rep_ques ="--> How many times do you want to send this text?"
+# print(rep_ques)
+# speak(rep_ques)
+# rep_ans = int(rec())                  #    "202" = 202
 
 # Switch to Whatsapp
 
